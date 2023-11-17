6cb75023691bbaed46a2c59b6126bb7e97ed86ec
zsz <zoltan.szaszi@gmail.com>
2023-11-17 07:55:16 -0600

    unit testing datasource config
    datasource config for unix socket is now optional

--- test/database_test.py
+++ test/database_test.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 
 from app import database
@@ -13,18 +11,11 @@ config = {
 }
 
 
-@pytest.fixture(params=[config])
-def mock_env_vars(request, monkeypatch):
-    print(request.param)
-    for key, val in request.param.items():
-        monkeypatch.setenv(key, val)
-
-
 @pytest.mark.parametrize(argnames=["config"], argvalues=[[config]])
-def test_datasource_init_from_env_vars(mock_env_vars, config):
+def test_datasource_config(config):
     datasource = database.DataSource(config)
-    assert datasource.get_host() == os.getenv(database.HOST)
-    assert datasource.get_user() == os.getenv(database.USER)
-    assert datasource.get_password() == os.getenv(database.PASSWORD)
-    assert datasource.get_database() == os.getenv(database.DATABASE)
-    assert datasource.get_unix_socket() == os.getenv(database.UNIX_SOCKET)
+    assert datasource.get_host() == config[database.HOST]
+    assert datasource.get_user() == config[database.USER]
+    assert datasource.get_password() == config[database.PASSWORD]
+    assert datasource.get_database() == config[database.DATABASE]
+    assert datasource.get_unix_socket() == config[database.UNIX_SOCKET]
